d7c5b80894571310a46fbe886fb81b3c381adb25
MaelJEAN <mael.jean@etu.mines-ales.fr>
2024-05-21 16:20:24 +0200

    TP1 fixed

--- TP1.c
+++ TP1.c
@@ -19,6 +19,7 @@ shared_mem superMalloc (int taille){
 
     m.descriptor = shmget(IPC_PRIVATE, taille, IPC_CREAT|IPC_EXCL|0600);
     m.adresse = (void*) - 1;
+    m.taille = taille;
     if (m.descriptor != -1){
         m.adresse = shmat(m.descriptor, NULL, 0);
     }
@@ -39,7 +40,7 @@ int superFree(shared_mem m){
 
 int creerSemaphore(int compteur){
     printf("creerSemaphore start");
-    int idSem = semget(IPC_PRIVATE, 1, 0600|IPC_CREAT|IPC_EXCL);
+    int idSem = semget(IPC_PRIVATE, 1, 0666|IPC_CREAT|IPC_EXCL);
     semctl(idSem, 0, SETVAL, compteur);
     printf("creerSemaphore end");
     return idSem;
@@ -53,41 +54,34 @@ int detruireSemaphore(int idSem){
 
 
 void P(int val){
-    printf("P start");
     struct sembuf sem;
     sem.sem_num = 0;
     sem.sem_op = -1;
     sem.sem_flg = 0;
-    printf("P end");
     return semop(val, &sem, 1);
 }
 
 void V(int val){
-    printf("V start");
     struct sembuf sem;
     sem.sem_num = 0;
     sem.sem_op = 1;
     sem.sem_flg = 0;
-    printf("V end");
     return semop(val, &sem, 1);
 }
 
 
 void placer(shared_mem mem, char lettre){
-    printf("placer start");
-    *(mem.adresse + mem.taille-1 - *mem.adresse) = lettre;
-    *mem.adresse = (char) ((int) *mem.adresse + 1);
-    printf("placer end");
+    *(mem.adresse + mem.taille-1 - (int) *(mem.adresse)) = lettre;
+    *mem.adresse = (char) ((int) *(mem.adresse) + 1);
 }
 
 char prendre(shared_mem mem){
-    printf("prendre start");
     char lettre = *(mem.adresse + mem.taille-1);
     for (int i = mem.taille-1; i > 1; i--){
+        printf("Boucle prendre : %d", i);
         *(mem.adresse+i) = *(mem.adresse+i-1);
     }
     *mem.adresse = (char) ((int) *mem.adresse - 1);
-    printf("prendre end");
     return lettre;
 }
 
@@ -109,7 +103,7 @@ void producteur(shared_mem mem_p, int mutex, int vide, int plein, char* message_
         V(plein);
 
         num_char++;
-    } while (lettre != "/0");
+    } while (lettre != '\0');
 
     printf("producteur end");
 }
@@ -131,27 +125,31 @@ void consomateur(shared_mem mem_p, int mutex, int vide, int plein, char* message
 
         message_to_receive[num_char] = lettre;
         num_char++;
-    } while (lettre != "/0");
+    } while (lettre != 0);
 
     printf("consomateur end");
 }
 
 int main(int argc, char const *argv[])
 {
+
     printf("1");
 
     int taille = 4;
     
     shared_mem mem = superMalloc(taille + 1);
-    int mutex = creerSemaphore(1);
     int plein = creerSemaphore(0);
     int vide = creerSemaphore(taille);
-    int etat;
-    char *message_to_send = "BONJOUR";
+    int mutex = creerSemaphore(1);
 
+    char *message_to_send = "BONJOUR";
+    
+    pid_t pid;
+    int etat;
+
+    pid = fork();
     printf("2");
 
-    pid_t pid = fork();
 
     switch (pid)
     {
@@ -167,7 +165,7 @@ int main(int argc, char const *argv[])
         char message_to_receive[40];
         consomateur(mem, mutex, vide, plein, message_to_receive);
         wait(&etat);
-        printf("Chaine Recue : %s", message_to_receive);
+        printf("\n\n Chaine Recue : %s \n\n", message_to_receive);
         superFree(mem);
         detruireSemaphore(mutex);
         detruireSemaphore(vide);
